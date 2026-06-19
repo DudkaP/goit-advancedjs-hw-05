701793a025ee30780c1c43babe28236e3bcabbe0
Dudka Pavlo <dudka.p@outlook.com>
2026-06-19 18:42:24 +0200

    фікс 3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { concatenation } from './concatenation';
 
-const button = document.querySelector('button')!;
-const input = document.querySelector('input')!;
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('button') as HTMLButtonElement;
+  const input = document.querySelector('input') as HTMLInputElement;
 
-if (button && input) {
-  button.addEventListener('click', () => {
-    concatenation(input.value, 'hello!');
-  });
-}
+  if (button && input) {
+    button.addEventListener('click', () => {
+      if (input.value.trim().length > 0) {
+        concatenation(input.value, 'hello!');
+      }
+    });
+  }
+});
